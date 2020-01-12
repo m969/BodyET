a518e96b23a6db41f9080e42af8ee99862e2179f
m969 <969041327@qq.com>
2020-01-12 23:49:21 +0800

    up

---
+++ Unity/Assets/Hotfix/Module/Demo/OperaComponent.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using ETModel;
+using UnityEngine;
+
+namespace ETHotfix
+{
+    [ObjectSystem]
+    public class OperaComponentAwakeSystem : AwakeSystem<OperaComponent>
+    {
+	    public override void Awake(OperaComponent self)
+	    {
+		    self.Awake();
+	    }
+    }
+
+	[ObjectSystem]
+	public class OperaComponentUpdateSystem : UpdateSystem<OperaComponent>
+	{
+		public override void Update(OperaComponent self)
+		{
+			self.Update();
+		}
+	}
+
+	public class OperaComponent: Entity
+    {
+        public Vector3 ClickPoint;
+
+	    public int mapMask;
+
+	    public void Awake()
+	    {
+		    this.mapMask = LayerMask.GetMask("Map");
+	    }
+
+	    private readonly Frame_ClickMap frameClickMap = new Frame_ClickMap();
+	    private readonly UnitOperation operationMsg = new UnitOperation();
+		private long lastSendTime;
+		public void Update()
+        {
+			if (TimeHelper.Now() - lastSendTime > 100)
+			{
+				lastSendTime = TimeHelper.Now();
+				if (Unit.LocalUnit == null)
+					return;
+				operationMsg.Index++;
+				operationMsg.Operation = 0;
+				var p = Unit.LocalUnit.Position;
+				operationMsg.X = (int)p.x;
+				operationMsg.Y = (int)p.y;
+				operationMsg.Z = (int)p.z;
+				ETModel.SessionComponent.Instance.Session.Send(operationMsg);
+			}
+			//        if (Input.GetMouseButtonDown(1))
+			//        {
+			//            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+			//            RaycastHit hit;
+			//         if (Physics.Raycast(ray, out hit, 1000, this.mapMask))
+			//         {
+			//	this.ClickPoint = hit.point;
+			//          frameClickMap.X = this.ClickPoint.x;
+			//          frameClickMap.Y = this.ClickPoint.y;
+			//          frameClickMap.Z = this.ClickPoint.z;
+			//          ETModel.SessionComponent.Instance.Session.Send(frameClickMap);
+
+			//	// 测试actor rpc消息
+			//	this.TestActor().Coroutine();
+			//}
+			//        }
+		}
+
+	    public async ETVoid TestActor()
+	    {
+		    try
+		    {
+			    M2C_TestActorResponse response = (M2C_TestActorResponse)await SessionComponent.Instance.Session.Call(
+						new C2M_TestActorRequest() { Info = "actor rpc request" });
+			    Log.Info(response.Info);
+			}
+		    catch (Exception e)
+		    {
+				Log.Error(e);
+		    }
+		}
+    }
+}
---
+++ Unity/Assets/Model/Module/Demo/UnitFactory.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+namespace ETModel
+{
+    public static class UnitFactory
+    {
+        public static Unit Create(Entity domain, long id)
+        {
+	        //var resourcesComponent = Game.Scene.GetComponent<ResourcesComponent>();
+	        //var bundleGameObject = (GameObject)resourcesComponent.GetAsset("Unit.unity3d", "Unit");
+	        //var prefab = bundleGameObject.Get<GameObject>("Skeleton");
+
+			//var go = UnityEngine.Object.Instantiate(prefab);
+			var unit = EntityFactory.CreateWithId<Unit, GameObject>(domain, id, GameObject.Find("ExampleCharacter"));
+
+			//unit.AddComponent<AnimatorComponent>();
+			//unit.AddComponent<MoveComponent>();
+			//unit.AddComponent<TurnComponent>();
+			//unit.AddComponent<UnitPathComponent>();
+
+			UnitComponent.Instance.Add(unit);
+            return unit;
+        }
+    }
+}
