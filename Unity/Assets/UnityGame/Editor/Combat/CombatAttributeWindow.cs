52b92fcead7b4a39e1d6de310497f4fd8e99ec9b
yangchengming <yangchengming@jtlxz.com>
2020-08-26 21:01:16 +0800

    up

--- Unity/Assets/UnityGame/Editor/Combat/CombatAttributeWindow.cs
+++ Unity/Assets/UnityGame/Editor/Combat/CombatAttributeWindow.cs
@@ -17,7 +17,7 @@
 		public ViewTool SomeField;
 
 		public List<AttributeConfig> AttributeConfigs = new List<AttributeConfig>();
-		[Button("+")]
+		//[Button("+")]
 		private void AddAttributeConfig()
 		{
 			var arr = System.DateTime.Now.Ticks.ToString().Reverse();
@@ -25,13 +25,54 @@
 		}
 
 		public List<StateConfig> StateConfigs = new List<StateConfig>();
-		[Button("+")]
+		//[Button("+")]
 		private void AddStateConfig()
 		{
 			var arr = System.DateTime.Now.Ticks.ToString().Reverse();
 			StateConfigs.Add(new StateConfig() { Guid = string.Concat(arr) });
 		}
 
+
+		[TableMatrix(HorizontalTitle = "Custom Cell Drawing", DrawElementMethod = "DrawColoredEnumElement", ResizableColumns = false, RowHeight = 16)]
+		public bool[,] CustomCellDrawing;
+
+		//[ShowInInspector, DoNotDrawAsReference]
+		//[TableMatrix(HorizontalTitle = "Transposed Custom Cell Drawing", DrawElementMethod = "DrawColoredEnumElement", ResizableColumns = false, RowHeight = 16, Transpose = true)]
+		//public bool[,] Transposed { get { return CustomCellDrawing; } set { CustomCellDrawing = value; } }
+
+		private static bool DrawColoredEnumElement(Rect rect, bool value)
+		{
+			if (Event.current.type == EventType.MouseDown && rect.Contains(Event.current.mousePosition))
+			{
+				value = !value;
+				GUI.changed = true;
+				Event.current.Use();
+			}
+
+			UnityEditor.EditorGUI.DrawRect(rect.Padding(1), value ? new Color(0.1f, 0.8f, 0.2f) : new Color(0, 0, 0, 0.5f));
+
+			return value;
+		}
+
+		[Button("Set")]
+		public void TransposeTableMatrixExample()
+		{
+			// =)
+			this.CustomCellDrawing = new bool[15, 15];
+			//this.CustomCellDrawing[6, 5] = true;
+			//this.CustomCellDrawing[6, 6] = true;
+			//this.CustomCellDrawing[6, 7] = true;
+			//this.CustomCellDrawing[8, 5] = true;
+			//this.CustomCellDrawing[8, 6] = true;
+			//this.CustomCellDrawing[8, 7] = true;
+			//this.CustomCellDrawing[5, 9] = true;
+			//this.CustomCellDrawing[5, 10] = true;
+			//this.CustomCellDrawing[9, 9] = true;
+			//this.CustomCellDrawing[9, 10] = true;
+			//this.CustomCellDrawing[6, 11] = true;
+			//this.CustomCellDrawing[7, 11] = true;
+			//this.CustomCellDrawing[8, 11] = true;
+		}
 
 
 		[MenuItem("Tools/UnityGame/战斗属性编辑界面")]
@@ -41,12 +82,10 @@
             window.position = GUIHelper.GetEditorWindowRect().AlignCenter(700, 700);
 		}
 
-        protected override void OnGUI()
-        {
-			base.OnGUI();
-
-
-        }
+   //     protected override void OnGUI()
+   //     {
+			//base.OnGUI();
+   //     }
     }
 
 	[Serializable]
