73471ec2ea5b889f1bcc571abbcbe1dc6fb19c9c
yangchengming <yangchengming@jtlxz.com>
2020-10-26 20:34:11 +0800

    up

---
+++ Unity/Assets/EGamePlay/Combat/CombatAction/CombatAction.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using EGamePlay;
+
+namespace EGamePlay.Combat
+{
+    /// <summary>
+    /// 战斗行为概念，造成伤害、治疗英雄、附加状态都属于战斗行为，需要继承自CombatAction
+    /// </summary>
+    public class CombatAction
+    {
+        public Entity ActionCreatorEntity { get; set; }
+        public List<Entity> ActionTargetEntities { get; set; }
+    }
+}
---
+++ Unity/Assets/EGamePlay/Combat/Entity/Entity.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace EGamePlay
+{
+    public class Entity
+    {
+        public Entity Parent { get; set; }
+        public List<Component> Components { get; set; }
+    }
+}
